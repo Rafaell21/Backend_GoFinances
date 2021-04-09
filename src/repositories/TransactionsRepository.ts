1cba8147dd1cf234e90caa89ef2f2a8e03a80679
Rafael Silva <rafael.silva@instaletecnologia.com.br>
2021-04-08 23:44:27 -0300

    Commit Final

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,6 +1,13 @@
-import { EntityRepository, Repository } from 'typeorm';
-
+/* eslint-disable no-param-reassign */
+import { EntityRepository, Repository, TransactionRepository } from 'typeorm';
+import { getCustomRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 interface Balance {
   income: number;
@@ -11,7 +18,29 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+    const { income, outcome } = transactions.reduce(
+      (acumulator, transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            acumulator.income += Number(transaction.value);
+            break;
+          case 'outcome':
+            acumulator.outcome += Number(transaction.value);
+            break;
+          default:
+            break;
+        }
+        return acumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    const total = income - outcome;
+    return { income, outcome, total };
   }
 }
 
